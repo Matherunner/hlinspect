eaa854de9c0408790b6ef3ba3d15ca4a39fffd0d
Chong Jiang Wei <randolphchong@gmail.com>
2021-02-14 17:07:37 +0800

    Add getting module information

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module hlinspect
 
 go 1.13
 
-require github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
+require (
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c
+)
--- hooks.go
+++ hooks.go
@@ -1,13 +1,63 @@
 package main
 
+import (
+	"errors"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
 /*
 #cgo 386 LDFLAGS: -Llib -lMinHook
 
+#include <windows.h>
 #include "MinHook.h"
 */
 import "C"
 
+func getModuleInfo(name string) (base uintptr, size uint, err error) {
+	type moduleInfo struct {
+		BaseOfDll   uintptr
+		SizeOfImage uint32
+		EntryPoint  uintptr
+	}
+
+	cname, err := windows.UTF16PtrFromString(name)
+	if err != nil {
+		return
+	}
+
+	var handle windows.Handle
+	err = windows.GetModuleHandleEx(0, cname, &handle)
+	if err != nil {
+		return
+	}
+	defer windows.FreeLibrary(handle)
+
+	psapi := windows.MustLoadDLL("psapi.dll")
+	getModuleInformation := psapi.MustFindProc("GetModuleInformation")
+	procHandle := windows.CurrentProcess()
+
+	var info moduleInfo
+	ret, _, _ := getModuleInformation.Call(uintptr(procHandle), uintptr(handle), uintptr(unsafe.Pointer(&info)), unsafe.Sizeof(info))
+	if ret == 0 {
+		err = errors.New("GetModuleInformation failed")
+		return
+	}
+
+	base = info.BaseOfDll
+	size = uint(info.SizeOfImage)
+	return
+}
+
 func initHooks() {
+	base, size, err := getModuleInfo("hw.dll")
+	if err != nil {
+		dllLog.Panic("Unable to get module info")
+	}
+
+	dllLog.Debugf("Got base and size of hw.dll: %v %v\n", base, size)
+
 	dllLog.Debug("Initializing hook")
 	if err := C.MH_Initialize(); err != C.MH_OK {
 		dllLog.Panicf("Unable to initialise hook: %v", err)
